d8fb0ea401b63d632c55c6f61b369021960ee6a2
Darc <darc@darcfx.com>
2026-05-24 00:19:50 -0400

    remove unused junk

--- later/publish-for-linux.sh
+++
@@ -1,9 +0,0 @@
-rm -rf ./publish/linux-x64/*
-dotnet publish src/JayBaldwin.AspNetCore/JayBaldwin.AspNetCore.csproj \
-  -c Release \
-  -r linux-x64 \
-  --self-contained true \
-  -o ./publish/linux-x64/app
-
-cp compose.prod.yml ./publish/linux-x64/
-cp restart.sh ./publish/linux-x64/
